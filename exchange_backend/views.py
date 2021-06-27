f8db1bede3c131e3ba0c9dd010f8f472788b75a6
Raviteja <raviteja.mallozala@ibigroup.com>
2021-06-27 23:52:37 +0530

    Transfer API views

--- exchange_backend/views.py
+++ exchange_backend/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import list_route
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from django.http import HttpResponseRedirect, HttpResponse
+from rest_framework.generics import get_object_or_404
 import requests
 from exchange_backend import filters
 from .serializers import *
@@ -31,12 +33,11 @@ class ExchangeService:
         url = f"{url}{settings.EXCHANGE_API['LIST_CURRENCIES_API']}"
         params = {
             "apiKey": {settings.EXCHANGE_API['API_TOKEN']}
-                  }
+        }
         req_obj = requests.get(url=url, params=params)
         currencies_list = []
         if req_obj.status_code == 200:
             data = req_obj.json()
-            print(data['results'])
             currencies_list = list(data['results'].keys()) if 'results' in data else []
         return currencies_list
 
@@ -53,9 +54,8 @@ class ExchangeService:
         req_obj = requests.get(url=url, params=params)
         if req_obj.status_code == 200:
             data = req_obj.json()
-            print(data)
-            # if converstion_string in data:
-            value = amount * data[converstion_string]
+            if converstion_string in data:
+                value = amount * data[converstion_string]
         return value
 
 
@@ -90,10 +90,82 @@ class WalletViewset(ExchangeModelViewSet):
         "currency_type"
     )
 
+    @staticmethod
+    def withdraw_money(user, currency_obj, amount):
+        if user.default_currency.id != currency_obj.id:
+            amount = ExchangeService.convert_currency(user.default_currency.name, currency_obj.name, amount)
+        validation = WalletViewset.validate_debit(user, amount)
+        if not validation:
+            raise ValidationError(f"Amount cannot be Withdrawn as Balance is lesser than withdrawing amount ")
+        tser = TransactionSerializer(data={
+            "user": user.id,
+            "dest_user_id": None,
+            "transaction_type": "debit",
+            "amount": -amount,
+            "currency_type": currency_obj.id,
+            "status": "accepted",
+            "description": None,
+        })
+        tser.is_valid(raise_exception=True)
+        tser.save()
+        wallet_obj = user.wallet
+        wallet_obj.current_balance -= amount
+        wallet_obj.save()
+        return
+
+    @staticmethod
+    def add_money(user, currency_obj, amount):
+        if user.default_currency.id != currency_obj.id:
+            amount = ExchangeService.convert_currency(user.default_currency.name, currency_obj.name, amount)
+        tser = TransactionSerializer(data={
+            "user": user.id,
+            "dest_user_id": None,
+            "transaction_type": "credit",
+            "amount": amount,
+            "currency_type": currency_obj.id,
+            "status": "accepted",
+            "description": None,
+        })
+        tser.is_valid(raise_exception=True)
+        tser.save()
+        wallet_obj = user.wallet
+        wallet_obj.current_balance += amount
+        wallet_obj.save()
+        return
+
+    @staticmethod
+    def transfer_money(from_user, to_user, amount, currency_obj):
+        if from_user.default_currency.id != currency_obj.id:
+            amount = ExchangeService.convert_currency(from_user.default_currency.name, currency_obj.name, amount)
+            validation = WalletViewset.validate_debit(from_user, amount)
+            if not validation:
+                raise ValidationError(f"Amount cannot be Transferred as Balance is lesser than transfering amount")
+            tser = TransactionSerializer(data={
+                "user": from_user.id,
+                "dest_user_id": to_user.id,
+                "transaction_type": "transfer",
+                "amount": amount,
+                "currency_type": currency_obj.id,
+                "status": "accepted",
+                "description": None,
+            })
+            tser.is_valid(raise_exception=True)
+            tser.save()
+            wallet_obj = user.wallet
+            wallet_obj.current_balance += amount
+            wallet_obj.save()
+
+    @classmethod
+    def validate_debit(cls, user, amount):
+        wallet = user.wallet
+        if wallet.current_balance < amount:
+            return False
+        return True
+
 
 class TransactionViewset(ExchangeModelViewSet):
     serializer_class = TransactionSerializer
-    queryset = Transaction.objects.all()
+    queryset = Transaction.objects.all()  # noqa
     filter_class = filters.TransactionFilter
     ordering_fields = (
         "id",
@@ -109,3 +181,40 @@ class TransactionViewset(ExchangeModelViewSet):
         "transaction_type",
         "currency_type",
     )
+
+    @transaction_atomic_db
+    @list_route(methods=['post'])
+    def wallet_transaction(self, request):
+        amount = request.data.get("amount", None)
+        currency_id = request.data.get("currency", None)
+        transaction_type = request.data.get("transaction_type", None)
+        if transaction_type not in Transaction.TRANSACTION_TYPE_CHOICES:
+            return Response(status=400, data={"message": "Transaction Invalid"})
+        curr_obj = get_object_or_404(Currency, pk=int(currency_id))
+        # if not curr_obj:
+        #     return Response(status=400, data={"message": "Transaction Invalid"})
+        status = None
+        if transaction_type == "debit":
+            WalletViewset.withdraw_money(request.user, curr_obj, amount)
+            # raise Exception
+            return Response(status=200,
+                            data={"message": "Money Successfully Debited from your Wallet"})
+        elif transaction_type == "credit":
+            WalletViewset.add_money(request.user, curr_obj, amount)
+            return Response(status=200,
+                            data={"message": "Money Successfully Credited to your Wallet"})
+
+    @transaction_atomic_db
+    @list_route(methods=['post'])
+    def transfer_money(self, request):
+        amount = request.data.get("amount", None)
+        currency_id = request.data.get("currency", None)
+        from_user_id = request.data.get("from_user_id", None)
+        to_user_id = request.data.get("to_user_id", None)
+        if not amount or not currency_id or not from_user_id or not to_user_id:
+            return Response(status=400,
+                            data={"message": "Bad Request"})
+        from_user = get_object_or_404(User, pk=int(from_user_id))
+        to_user = get_object_or_404(User, pk=int(to_user_id))
+        curr_obj = get_object_or_404(Currency, pk=int(currency_id))
+        WalletViewset.transfer_money(from_user, to_user, amount, curr_obj)
--- settings.py
+++ settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "django_extensions",
     "rest_framework",
+    "rest_framework.authtoken",
     "django_filters",
     "exchange_backend",
-
 
 ]
 
@@ -57,6 +56,17 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'urls'
+
+REST_FRAMEWORK = {
+    "PAGE_SIZE": 100,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "misc.IsAuthenticatedOrOptions",
+    ),
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
+}
 
 TEMPLATES = [
     {
@@ -76,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -87,7 +96,6 @@ DATABASES = {
     }
 }
 AUTH_USER_MODEL = 'exchange_backend.User'
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -107,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -121,11 +128,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = "/login/"
+LOGOUT_URL = "/logout/"
+LOGIN_REDIRECT_URL = "/"
 
 STATIC_ROOT = os.path.join(PROJECT_PATH, "deployment_static")
 # STATICFILES_DIRS = [
